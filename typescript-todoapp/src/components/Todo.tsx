ec26cd11d6bf99cb7b6fb448499bdf28206a4c57
OguzBerkayBAL <l21210320341@stud.sdu.edu.tr>
2025-01-31 15:04:33 +0300

    feat: add removeTodoById function to remove todos by ID

--- typescript-todoapp/src/components/Todo.tsx
+++ typescript-todoapp/src/components/Todo.tsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import { IoMdRemoveCircleOutline } from 'react-icons/io';
-import { FaCheck } from 'react-icons/fa';
-import { FaRegEdit } from 'react-icons/fa';
+import React from "react";
+import { IoMdRemoveCircleOutline } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
+import { TodoType } from "../types/Types";
+import { useDispatch } from "react-redux";
+import { removeTodoById } from "../redux/todoSlice";
 
-
-const Todo = () => {
-    return (
-        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px', border: '1px solid #ccc', borderRadius: '5px', marginTop: '25px'}}>
-            <div>ben ilk todoyum</div>
-            <div>
-                <IoMdRemoveCircleOutline className='icons' style={{marginRight:'10px'}}/>
-                <FaRegEdit className='icons'/>
-            </div>
-        </div>
-    )
+interface TodoProps {
+  todoProps: TodoType;
 }
 
-export default Todo
+const Todo = ({ todoProps }: TodoProps) => {
+  const { id, content } = todoProps;
+
+  const dispatch = useDispatch();
+
+  const handleRemoveTodo = () => {
+    dispatch(removeTodoById(id));
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "16px",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        marginTop: "25px",
+      }}
+    >
+      <div>{content}</div>
+      <div>
+        <IoMdRemoveCircleOutline
+          onClick={handleRemoveTodo}
+          className="icons"
+          style={{ marginRight: "10px" }}
+        />
+        <FaRegEdit className="icons" />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
