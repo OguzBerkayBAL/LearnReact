0b6f90a8952e0d92ef8bb0dfd57e3c42c83c912b
OguzBerkayBAL <oguzberkaybal@icloud.com>
2025-02-06 15:47:48 +0300

    fix

--- typescript-todoapp/src/main.tsx
+++ typescript-todoapp/src/main.tsx
@@ -1,16 +1,16 @@
 // src/index.tsx veya main.tsx
 import React from "react";
-import ReactDOM from "react-dom";
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { store, persistor } from "./redux/store";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>,
-  document.getElementById("root")
-);
+)
