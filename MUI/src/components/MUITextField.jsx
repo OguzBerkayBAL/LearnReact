637a54f8368781ac841ca47ab63c681fe93aff51
OguzBerkayBAL <oguzberkaybal@icloud.com>
2025-02-09 20:56:17 +0300

    up

--- MUI/src/components/MUITextField.jsx
+++ MUI/src/components/MUITextField.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import PersonIcon from '@mui/icons-material/Person'
 
 const MUITextField = () => {
   return (
     <div>
       <div style={{ marginBottom: "50px" }}>
-        <TextField  label="Standard" variant="standard" />
+        <TextField label="Standard" variant="standard" />
 
         <TextField label="Filled" variant="filled" />
 
-        <TextField  label="Outlined" variant="outlined" />
+        <TextField label="Outlined" variant="outlined" />
       </div>
 
       <div style={{ marginBottom: "50px" }}>
@@ -72,6 +74,36 @@ const MUITextField = () => {
           helperText="Success"
         />
       </div>
+
+      <div style={{ marginBottom: "50px" }}>
+        <TextField label='Kimlik No' disabled variant='outlined' color='warning' />
+      </div>
+
+      <div style={{ marginBottom: "50px" }}>
+        <TextField InputProps={{readOnly:true}} label='Kimlik No' value={'berkay'} disabled variant='outlined' color='warning' />
+      </div>
+
+      <div style={{ marginBottom: "50px" }}>
+        <TextField
+        label='Kimlik No'
+        
+        variant='outlined'
+        slotProps={{
+          input: {
+            endAdornment: ( //başta olsun istersen startAdornment
+              <InputAdornment position="end">
+                <PersonIcon />
+              </InputAdornment>
+            )
+          }
+        }} 
+         ></TextField>
+      </div>
+
+      <div style={{ marginBottom: "50px" }}>
+        <TextField label='Kimlik No' variant='outlined' size='small' />
+      </div>
+
     </div>
   );
 }
